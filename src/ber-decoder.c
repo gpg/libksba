426f93b876eba053d25a83d032a90a6796595a70
Werner Koch <wk@gnupg.org>
2003-08-20 17:27:42 +0000

    Applied Macrus valgrinded fix.

--- src/ber-decoder.c
+++ src/ber-decoder.c
@@ -91,10 +91,11 @@ new_decoder_state (void)
   ds->stacksize = 100;
   ds->idx = 0;
   ds->cur.node = NULL;
+  ds->cur.went_up = 0;
   ds->cur.in_seq_of = 0;
+  ds->cur.in_any = 0;
   ds->cur.again = 0;
   ds->cur.next_tag = 0;
-  ds->cur.went_up = 0;
   ds->cur.length = 0;
   ds->cur.ndef_length = 1;
   ds->cur.nread = 0;
