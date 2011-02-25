be7fbc8352de83de7715b0fb7986dbe59b6b56ba
Werner Koch <wk@gnupg.org>
2011-02-25 09:51:46 +0100

    Build t-oid and add a feature to translate an oid to hex.

--- tests/t-oid.c
+++ tests/t-oid.c
@@ -72,18 +72,46 @@ read_into_buffer (FILE *fp, size_t *r_length)
 int
 main (int argc, char **argv)
 {
-  char *buffer;
-  size_t buflen;
-  char *result;
+  gpg_error_t err;
+  if (argc)
+    {
+      argc--;
+      argv++;
+    }
 
-  (void)argc;
-  (void)argv;
+  if (argc)
+    {
+      unsigned char *buffer;
+      size_t n, buflen;
 
-  buffer = read_into_buffer (stdin, &buflen);
-  result = ksba_oid_to_str (buffer, buflen);
-  free (buffer);
-  printf ("%s\n", result? result:"[malloc failed]");
-  free (result);
+      for ( ;argc ; argc--, argv++)
+        {
+          err = ksba_oid_from_str (*argv, &buffer, &buflen);
+          if (err)
+            {
+              fprintf (stderr, "can't convert `%s': %s\n",
+                       *argv, gpg_strerror (err));
+              return 1;
+            }
+          printf ("%s ->", *argv);
+          for (n=0; n < buflen; n++)
+            printf (" %02X", buffer[n]);
+          putchar ('\n');
+          free (buffer);
+        }
+    }
+  else
+    {
+      char *buffer;
+      size_t buflen;
+      char *result;
+
+      buffer = read_into_buffer (stdin, &buflen);
+      result = ksba_oid_to_str (buffer, buflen);
+      free (buffer);
+      printf ("%s\n", result? result:"[malloc failed]");
+      free (result);
+    }
 
   return 0;
 }
